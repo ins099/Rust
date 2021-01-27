b42f81af85eeb9c6efc5cd4fc052c70852082793
ins099 <alaminsaram92@gmail.com.com>
2021-01-27 15:19:38 +0500

    Hello Rust!

---
+++ play_ground/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("Hello, world!");
+
+    let a = 1 +20;
+
+    let s = 30 - 20;
+    let m = 5 * 20;
+    let d = 20 / 5 ;
+    let r = 49 % 2;
+
+    //bool: true/false
+
+    let _c = "character";
+
+    let _ch: char = 'z';
+
+
+    println!("{} {} {} {} {}",a,s,m,d,r);
+
+}
